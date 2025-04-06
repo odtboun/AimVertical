c2a0d6e0bbc24c62a12538cdf314bf301d0cc047
odtboun <odtboun@users.noreply.github.com>
2025-04-06 04:15:45 +0300

    fix: add direct link tags for favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`scroll-smooth ${plusJakarta.variable}`}>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body className={inter.className} suppressHydrationWarning>{children}</body>
     </html>
   );
